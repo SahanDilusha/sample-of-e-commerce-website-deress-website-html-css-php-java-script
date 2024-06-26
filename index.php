13a83b8c819de87bbdf50bcaeca3f28646d57aca
SahanDilusha <sahancodelygroup@gmail.com>
2024-06-26 20:33:26 +0530

    "Removed tetst.html file and made minor changes to index.php"

--- index.php
+++ index.php
@@ -10,9 +10,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
     <link rel="stylesheet" href="style.css" />
 
+   
 </head>
 
 <body onload="startCountdown();">
+
+
 
     <?php
     include "connecton.php";
